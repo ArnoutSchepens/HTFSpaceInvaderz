cf2b06050a5e865e4d343c41f191cbfa07551907
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-12-08 13:02:32 +0100

    Commit

--- Button_And_Buzzer.py
+++ Button_And_Buzzer.py
@@ -6,21 +6,20 @@ import requests
 buzzer_pin = 2
 button = 4
 buzzerOn = False
-buttonPrevious = False
+previousInput = 0
 linkAPI = 'http://192.168.50.148:4000/api'
 
 pinMode(buzzer_pin, 'OUTPUT')
 pinMode(button, 'INPUT')
-r = requests.get(linkAPI)
+"""r = requests.get(linkAPI)"""
 while True:
     try:
         button_status = digitalRead(button)
-        if button_status:
-            buttonPrevious = True
+        if ((not previousInput) and button_status):
+            digitalWrite(buzzer_pin, 1)
             previousInput = button_status
         else:
             digitalWrite(buzzer_pin,0)
-            previousInput != button_status
     except KeyboardInterrupt:
         digitalWrite(buzzer_pin, 0)
     except (IOError, TypeError) as e:
