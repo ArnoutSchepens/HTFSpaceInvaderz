ed8303997a234edf531831caca503634a3da9dfd
Lanzanotte <lanzanotte@gmail.com>
2016-12-08 12:30:38 +0100

    Rollback working version

--- Button_And_Buzzer.py
+++ Button_And_Buzzer.py
@@ -13,17 +13,12 @@ pinMode(button, 'INPUT')
 while True:
     try:
         button_status = digitalRead(button)
-        if button_status:
-            buttonPrevious = True
-        else:
-            buttonPrevious = False
-        while buttonPrevious != button_status:
-            if button_status and buzzerOn == True:
-                digitalWrite(buzzer_pin, 0)
-                buzzerOn = False
-            elif button_status and buzzerOn == False:
-                digitalWrite(buzzer_pin, 1)
-                buzzerOn = True
+        if button_status and buzzerOn == True:
+            digitalWrite(buzzer_pin, 0)
+            buzzerOn = False
+        elif button_status and buzzerOn == False:
+            digitalWrite(buzzer_pin, 1)
+            buzzerOn = True
     except KeyboardInterrupt:
         digitalWrite(buzzer_pin, 0)
     except (IOError, TypeError) as e:
