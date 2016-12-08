739a4927bba7338bb6b4a6dbda255b848c68384a
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-12-08 12:54:26 +0100

    Request

--- Button_And_Buzzer.py
+++ Button_And_Buzzer.py
@@ -1,37 +1,32 @@
 import time
 from grovepi import *
 import math
+import requests
 
 buzzer_pin = 2
 button = 4
 buzzerOn = False
-previousInput = 0
+buttonPrevious = False
+linkAPI = 'http://192.168.50.148:4000/api'
 
 pinMode(buzzer_pin, 'OUTPUT')
 pinMode(button, 'INPUT')
-
+r = requests.get(linkAPI)
 while True:
     try:
         button_status = digitalRead(button)
-        if ((not previousInput) and button_status):
-            digitalWrite(buzzer_pin,1)
-            previousInput = button_status
+        if button_status:
+            buttonPrevious = True
         else:
-            digitalWrite(buzzer_pin,0)
-
+            buttonPrevious = False
+        while buttonPrevious != button_status:
+            if button_status and buzzerOn == True:
+                digitalWrite(buzzer_pin, 0)
+                buzzerOn = False
+            elif button_status and buzzerOn == False:
+                digitalWrite(buzzer_pin, 1)
+                buzzerOn = True
     except KeyboardInterrupt:
         digitalWrite(buzzer_pin, 0)
     except (IOError, TypeError) as e:
         print "Error"
-
-
-
-"""
-        button_status = digitalRead(button)
-        if button_status and buzzerOn == True:
-            digitalWrite(buzzer_pin, 0)
-            buzzerOn = False
-        elif button_status and buzzerOn == False:
-            digitalWrite(buzzer_pin, 1)
-            buzzerOn = True
-"""
