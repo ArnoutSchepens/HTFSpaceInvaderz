07d1d46d7c4915c6a873c13645e1ba67d090986d
ArnoutSchepens <schepens.arnout@gmail.com>
2016-12-08 12:45:42 +0100

    Buzzer only changing on push

--- Button_And_Buzzer.py
+++ Button_And_Buzzer.py
@@ -5,7 +5,7 @@ import math
 buzzer_pin = 2
 button = 4
 buzzerOn = False
-buttonPrevious = False
+previousInput = 0
 
 pinMode(buzzer_pin, 'OUTPUT')
 pinMode(button, 'INPUT')
@@ -13,13 +13,25 @@ pinMode(button, 'INPUT')
 while True:
     try:
         button_status = digitalRead(button)
+        if ((not previousInput) and button_status):
+            digitalWrite(buzzer_pin,1)
+            previousInput = button_status
+        else:
+            digitalWrite(buzzer_pin,0)
+
+    except KeyboardInterrupt:
+        digitalWrite(buzzer_pin, 0)
+    except (IOError, TypeError) as e:
+        print "Error"
+
+
+
+"""
+        button_status = digitalRead(button)
         if button_status and buzzerOn == True:
             digitalWrite(buzzer_pin, 0)
             buzzerOn = False
         elif button_status and buzzerOn == False:
             digitalWrite(buzzer_pin, 1)
             buzzerOn = True
-    except KeyboardInterrupt:
-        digitalWrite(buzzer_pin, 0)
-    except (IOError, TypeError) as e:
-        print "Error"
+"""
