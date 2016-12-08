a839bd51f145a5e4fa1490010fc02c54b38b3c75
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-12-08 13:10:40 +0100

    REST

--- Button_And_Buzzer.py
+++ Button_And_Buzzer.py
@@ -11,7 +11,8 @@ linkAPI = 'http://192.168.50.148:4000/api'
 
 pinMode(buzzer_pin, 'OUTPUT')
 pinMode(button, 'INPUT')
-"""r = requests.get(linkAPI)"""
+r = requests.post(linkAPI, data = {'team':{{'id' : 13}}, 'sensor':{{'id': 0}, {'state':True}, {'value':'WARNING!'}}})
+print(r.url)
 while True:
     try:
         button_status = digitalRead(button)
